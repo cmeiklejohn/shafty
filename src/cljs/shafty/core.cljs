8d5bb87c52303c04ddc920b3239844425673f6b2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-12 01:33:23 -0500

    Completely refactor behaviours and add events.

--- src/cljs/shafty/core.cljs
+++ src/cljs/shafty/core.cljs
@@ -1,41 +1,57 @@
 (ns shafty.core)
 
-(defprotocol IReactive
-  "Event occurance handler."
-  (receive-event! [this occurrence]
-            "Receive a particular event occurrence and apply behavior to
-            event occurrence to generate a new behavior."))
+(defprotocol IRawEventReceiver
+  "The raw event receiver interface provides a mechanism to generate
+  functions which can be the target of DOM, XHR or other events which
+  add an object to the event stream."
+  (generate-receiver [this select-fn]
+                      "Generate a receiver function which can be used as
+                      the target of an addEventReceiver, or an
+                      XmlHttpRequest response.  When called, the result
+                      of the select-fn applied to the event will be
+                      added to the event stream."))
 
-(deftype Behavior [state meta watches]
-  IDeref
-  (-deref [_] (apply state []))
+(defprotocol IEventStream
+  "The event stream interface provides a series of filtering and
+  selection methods for working with objects as they enter and leave the
+  event stream"
+  (filter! [this filter-fn]
+           "Filter the objects of the event stream using the provided
+           filter-fn")
+  (map! [this map-fn]
+           "Map the objects of the event stream using the provided
+           map"))
 
-  IMeta
-  (-meta [_] meta)
+(defprotocol IBehaviourConversion
+  "Convert an event stream into a behaviour initializing with a default value"
+  (hold! [this init]
+         "Given an initial value, create a behaviour from an event stream."))
 
-  IWatchable
-  (-notify-watches [this oldval newval]
-    (doseq [[key f] watches]
-      (f key this oldval newval)))
-  (-add-watch [this key f]
-    (set! (.-watches this) (assoc watches key f)))
-  (-remove-watch [this key]
-    (set! (.-watches this) (dissoc watches key)))
-
-  IReactive
-  (receive-event! [this occurrence]
-    (swap! this (fn [] (apply state [occurrence])))))
-
-(defn behavior
-  "Define a behavior."
-  ([x] (Behavior. x nil nil)))
-
-(deftype Lift [behavior name value-fn state meta watches]
+(deftype Behaviour [state watches]
   IDeref
   (-deref [_] state)
 
-  IMeta
-  (-meta [_] meta)
+  IRawEventReceiver
+  (generate-receiver [this select-fn]
+    (fn [event] (swap! this #(apply select-fn [event]))))
+
+  IEventStream
+  (filter! [this filter-fn]
+    (let [e (Behaviour. nil nil)]
+      (-add-watch this (gensym "event")
+                  (fn [x y a b]
+                    (let [r (apply filter-fn [b])]
+                      (if (true? r)
+                        (swap! e #(identity b))))))
+      e))
+
+  (map! [this map-fn]
+    (let [e (Behaviour. nil nil)]
+      (-add-watch this (gensym "event")
+                  (fn [x y a b]
+                    (swap! e #(apply map-fn [b]))))
+      e))
+
 
   IWatchable
   (-notify-watches [this oldval newval]
@@ -46,12 +62,52 @@
   (-remove-watch [this key]
     (set! (.-watches this) (dissoc watches key))))
 
-(defn lift
-  "Lift a normal function onto a behavior."
-  ([behavior name value-fn]
-   (let [lift (Lift. behavior name value-fn nil nil nil)]
-     (-add-watch behavior name (fn [x y a b]
-                                 (let [v (apply b [])]
-                                  (swap! lift (fn [] (apply value-fn [v]))))))
-     (swap! lift (constantly (apply value-fn [@behavior])))
-     lift)))
+(deftype Event [watches]
+  IRawEventReceiver
+  (generate-receiver [this select-fn]
+    (fn [event] (swap! this #(apply select-fn [event]))))
+
+  IBehaviourConversion
+  (hold! [this init]
+    (.log js/console (str "init at " init))
+    (let [be (Behaviour. init nil)]
+      (-add-watch this (gensym "behaviour")
+                  (fn [x y a b]
+                    (swap! be (constantly b))))
+      be))
+
+  IEventStream
+  (filter! [this filter-fn]
+    (let [ev (Event. nil)]
+      (-add-watch this (gensym "event")
+                  (fn [x y a b]
+                    (let [r (apply filter-fn [b])]
+                      (if (true? r)
+                        (swap! ev #(identity b))))))
+      ev))
+
+  (map! [this map-fn]
+    (let [ev (Event. nil)]
+      (-add-watch this (gensym "event")
+                  (fn [x y a b]
+                    (swap! ev #(apply map-fn [b]))))
+      ev))
+
+  IWatchable
+  (-notify-watches [this oldval newval]
+    (doseq [[key f] watches]
+      (f key this oldval newval)))
+  (-add-watch [this key f]
+    (set! (.-watches this) (assoc watches key f)))
+  (-remove-watch [this key]
+    (set! (.-watches this) (dissoc watches key))))
+
+(defn behaviour
+  "Define a behaviour, which is a time-varying value providing constant
+  values."
+  ([x] (Behaviour. x nil)))
+
+(defn event
+  "Define an event, which is a time-varying value with finite
+  occurences."
+  ([] (Event. nil)))
--- test/integration/integration.cljs
+++ test/integration/integration.cljs
@@ -1,1 +1,52 @@
-(ns test.integration.integration)
+(ns test.integration.integration
+  (:require [shafty.core :as shafty]))
+
+(.log js/console "Starting Tests")
+
+;; Create an event.
+(def my-event (shafty/event))
+
+;; Generate a receiver, and send events to it.
+(def my-event-receiver (shafty/generate-receiver my-event
+                                          (fn [x]
+                                            (.log js/console (str "Event selector called with " x))
+                                            (identity x))))
+
+;; Filter events into a new stream that are equal to the value 1.
+(def my-filtered-event (shafty/filter! my-event
+                                (fn [x]
+                                  (.log js/console (str "Filter called with " x))
+                                  (= 1 x))))
+
+;; Map events into a new stream with the identity of 1.
+(def my-mapped-event (shafty/map! my-event
+                           (fn [x]
+                             (.log js/console (str "Map called with " x))
+                             (identity 1))))
+
+;; Map mapped into a new stream and perform assertions that all received values
+;; are 1.
+(def my-secondary-mapped-event (shafty/filter! my-mapped-event
+                                        (fn [x]
+                                          (.log js/console (str "Secondary map called with " x))
+                                          (assert (= 1 x)))))
+
+;; Map filtered into a new stream and perform assertions that all received values
+;; are 1.
+(def my-secondary-filtered-event (shafty/filter! my-mapped-event
+                                          (fn [x]
+                                            (.log js/console (str "Secondary filter called with " x))
+                                            (assert (= 1 x)))))
+
+;; Convert event stream into a behavior.
+(def my-behaviour-of-ones (shafty/hold! my-filtered-event 2))
+
+;; Send events.
+(my-event-receiver 1)
+(my-event-receiver 2)
+(my-event-receiver 1)
+
+;; Assert behavior has the correct value.
+(assert (= 1 @my-behaviour-of-ones))
+
+(.log js/console "Ending Tests")
