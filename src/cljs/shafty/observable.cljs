abb84d1125437badad3743aa4fddf3497408e308
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-17 23:37:42 -0500

    Add generic functions for items where we don't have a type to extend.

--- src/cljs/shafty/observable.cljs
+++ src/cljs/shafty/observable.cljs
@@ -8,14 +8,25 @@
   (bind! [this x]
          "Bind a given browser event or element."))
 
-(extend-type js/Node
+(extend-type js/HTMLButtonElement
   Observable
   (bind! [this event]
     (let [e (event/event)]
       (events/listen this event (fn [ev]
                                   (-notify-watches e nil ev))) e)))
+
+;; Generic functions for elements or events where we don't have a
+;; protocol.
+;;
 (defn bind-timer! [interval]
   "Generate a new timer at a given interval, and bind to a new event
   stream."
   (let [e (event/event)]
     (js/setInterval (fn [] (-notify-watches e nil (js/Date))) interval) e))
+
+(defn bind-event! [selector event]
+  "Given a selector and an event, bind an listener to a new event
+  stream."
+  (let [e (event/event)
+        element (dom/getElement selector)]
+    (events/listen element event (fn [ev] (-notify-watches e nil ev))) e))
