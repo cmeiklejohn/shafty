50a464ed881995c69009e4138fb5967523bb1ed7
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-18 01:53:50 -0500

    Activate two-way bindings on behaviours.
    * Reduce to build more complex behavioural streams.
    * Provide get/set fn's for binding population.
    * Add helper for textareas.

--- src/cljs/shafty/examples/autosave.cljs
+++ src/cljs/shafty/examples/autosave.cljs
@@ -1,14 +1,19 @@
 (ns shafty.examples.autosave
-  (:use [shafty.observable :only [bind! bind-timer!]]
+  (:use [shafty.observable :only [bind! bind-timer! bind-behaviour!]]
         [shafty.event_stream :only [merge! map!]])
   (:require [goog.dom :as dom]))
+
+(defn- update-save-status []
+  (let [element (dom/getElement "save-status")
+        curtime (js/Date)]
+    (set! (.-innerHTML element) (str "Last save at " curtime))))
 
 (defn main []
   "Run the autosave example"
 
-  (let [as (fn [] (.log js/console "Autosave function firing!"))
-        e1 (bind-timer! 5000)
+  (let [e1 (bind-timer! 5000)
         e2 (bind! (dom/getElement "save-button") "click")
         e3 (merge! e1 e2)
-        e4 (map! e3 as)]
+        e4 (map! e3 update-save-status)
+        b1 (bind! (dom/getElement "data"))]
     (.log js/console "Running autosave example.")))
--- src/cljs/shafty/observable.cljs
+++ src/cljs/shafty/observable.cljs
@@ -1,32 +1,52 @@
 (ns shafty.observable
+  (:use [shafty.event_stream :only [merge!]]
+        [shafty.behaviour_conversion :only [hold!]])
   (:require [shafty.event :as event]
+            [shafty.behaviour :as behaviour]
             [goog.dom :as dom]
             [goog.events :as events]))
 
-(defprotocol Observable
-  "Generate observables from browser elements or events."
-  (bind! [this x]
-         "Bind a given browser event or element."))
-
-(extend-type js/HTMLButtonElement
-  Observable
-  (bind! [this event]
-    (let [e (event/event)]
-      (events/listen this event (fn [ev]
-                                  (-notify-watches e nil ev))) e)))
-
-;; Generic functions for elements or events where we don't have a
-;; protocol.
-;;
-(defn bind-timer! [interval]
+(defn bind-timer!
   "Generate a new timer at a given interval, and bind to a new event
   stream."
-  (let [e (event/event)]
-    (js/setInterval (fn [] (-notify-watches e nil (js/Date))) interval) e))
+  ([interval]
+   (bind-timer! interval js/Date))
+  ([interval value-fn]
+   (let [e (event/event)]
+     (js/setInterval (fn []
+                       (-notify-watches e nil (value-fn))) interval) e)))
 
-(defn bind-event! [selector event]
-  "Given a selector and an event, bind an listener to a new event
+(defn bind-event! [element event value-fn]
+  "Given a selector and an event, bind an event listener to a new event
   stream."
-  (let [e (event/event)
-        element (dom/getElement selector)]
-    (events/listen element event (fn [ev] (-notify-watches e nil ev))) e))
+    (let [e (event/event)]
+      (events/listen element event (fn [ev]
+                                     (-notify-watches e nil (apply value-fn [ev])))) e))
+
+(defn bind-behaviour! [element events get-fn set-fn]
+  "Given a selector, generate an onchange event stream for the
+  particular element, and return a new behaviour."
+  (let [e (reduce (fn [acc x] (merge! acc x))
+                  (map (fn [x] (bind-event! element x get-fn)) events))
+        value (get-fn)
+        b (hold! e value)]
+    (-add-watch b (gensym "watch") (fn [x y a b] (set-fn b))) b))
+
+(defprotocol Observable
+  "Generate observables from browser elements or events."
+  (bind! [this] [this event]
+         "Given a particular element type, bind a listener for a
+         particular type of event and return an event stream which will
+         be populated with events."))
+
+(extend-type js/HTMLElement
+  Observable
+  (bind! [this event]
+    (bind-event! this event (fn [x] (identity x)))))
+
+(extend-type js/HTMLTextAreaElement
+  Observable
+  (bind! [this]
+    (bind-behaviour! this ["change" "keyup"]
+                     (fn [] (.-value this))
+                     (fn [x] (set! (.-value this) x)))))
