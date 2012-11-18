a23da02fe28b02c70670a997ab608b5175c6b699
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-17 22:54:35 -0500

    Add shorthands for binding event handlers and timers.

--- src/cljs/shafty/core.cljs
+++ src/cljs/shafty/core.cljs
@@ -1,1 +1,18 @@
-(ns shafty.core)
+(ns shafty.core
+  (:require [shafty.event :as event]
+            [goog.events :as events]
+            [goog.dom :as dom]))
+
+(defn bind-timer! [interval]
+  "Generate a new timer at a given interval, and bind to a new event
+  stream."
+  (let [e (event/event)]
+    (js/setInterval (fn [] (-notify-watches e nil (js/Date))) 1000) e))
+
+(defn bind-event! [selector event-type]
+  "Given a selector and an event-type, bind an listener to a new event
+  stream."
+  (let [e (event/event)
+        element (dom/getElement selector)]
+    (events/listen element event-type (fn [ev]
+                                        (-notify-watches e nil ev))) e))
--- src/cljs/shafty/examples/autosave.cljs
+++ src/cljs/shafty/examples/autosave.cljs
@@ -1,24 +1,13 @@
 (ns shafty.examples.autosave
-  (:use [shafty.event :only [event]]
-        [shafty.event_stream :only [merge! map!]])
-  (:require [goog.events :as events]
-            [goog.dom :as dom]))
+  (:use [shafty.core :only [bind-event! bind-timer!]]
+        [shafty.event_stream :only [merge! map!]]))
 
 (defn main []
   "Run the autosave example"
-  (.log js/console "Running autosave example.")
 
-  (let [ef (fn [] (js/Date))
-        as (fn [] (.log js/console "Autosave function firing!"))
-        e1 (event)
-        e2 (event)
+  (let [as (fn [] (.log js/console "Autosave function firing!"))
+        e1 (bind-timer! 1000)
+        e2 (bind-event! "save-button" "click")
         e3 (merge! e1 e2)
         e4 (map! e3 as)]
-
-    ;; Bind the event receiver to a timer.
-    (js/setInterval (fn [] (-notify-watches e1 nil (ef))) 1000)
-
-    ;; Bind event receiver to the save button.
-    (let [element (dom/getElement "save-button")]
-      (events/listen
-        element "click" (fn [] (-notify-watches e2 nil (ef)))))))
+    (.log js/console "Running autosave example.")))
