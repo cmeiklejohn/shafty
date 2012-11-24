e9fa294f62be363b5d8e36d52698187a0335e76e
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-23 20:22:32 -0500

    Add LICENSE.

--- src/cljs/shafty/event.cljs
+++ src/cljs/shafty/event.cljs
@@ -1,3 +1,13 @@
+;; Copyright (c) Christopher Meiklejohn. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0
+;; (http://opensource.org/licenses/eclipse-1.0.php) which can be found
+;; in the file LICENSE.html at the root of this distribution.  By using this
+;; software in any fashion, you are agreeing to be bound by the terms of
+;; this license. You must not remove this notice, or any other, from
+;; this software.
+;;
 (ns shafty.event
   (:use [shafty.behaviour-conversion :only [BehaviourConversion]]
         [shafty.event-stream :only [EventStream propagate!]]
@@ -26,8 +36,8 @@
 
 (extend-type Event
   BehaviourConversion
-  (hold! [this init]
-    (let [b (behaviour init this)]
+  (hold! [this initial]
+    (let [b (behaviour initial this)]
       (set! (.-sinks this) (conj (.-sinks this) b)) b)))
 
 (extend-type Event
--- src/cljs/shafty/examples/autosave.cljs
+++ src/cljs/shafty/examples/autosave.cljs
@@ -1,3 +1,17 @@
+;; Copyright (c) Christopher Meiklejohn. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0
+;; (http://opensource.org/licenses/eclipse-1.0.php) which can be found
+;; in the file LICENSE.html at the root of this distribution.  By using this
+;; software in any fashion, you are agreeing to be bound by the terms of
+;; this license. You must not remove this notice, or any other, from
+;; this software.
+;;
+;; Autosave example.
+;;
+;; Example similar to the example in Section 2.3 of the Flapjax paper.
+;;
 (ns shafty.examples.autosave
   (:use [shafty.observable :only [bind! bind-timer!]]
         [shafty.event-stream :only [merge! map! snapshot!]])
