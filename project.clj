e69a060fa3f6617a05a6bb9d00245efba6645d5a
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-11-29 18:05:58 -0500

    Add copyright and update title.

--- project.clj
+++ project.clj
@@ -1,5 +1,14 @@
+;; Copyright (c) Christopher Meiklejohn. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 which can be found in the file
+;; LICENSE.html at the root of this distribution.  By using this
+;; software in any fashion, you are agreeing to be bound by the terms of
+;; this license. You must not remove this notice, or any other, from
+;; this software.
+;;
 (defproject shafty "0.0.1-SNAPSHOT"
-  :description "Prototype ClojureScript functional reactive programming library."
+  :description "ClojureScript functional reactive programming library."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :source-paths ["src/clj" "src/cljs"]
